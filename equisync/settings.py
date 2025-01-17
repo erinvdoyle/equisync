223564d7c0bd121b0904095b43d18fb9999b43a9
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-17 23:44:42 +0000

    update code for Heroku deployment, relates to #13

--- equisync/settings.py
+++ equisync/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g79sm9=ln*cing+$03b@@y)y2($v2hod%66y8ua_=1qa#15s4x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-erinvdoyle-equisync-uqcw06qmtq2.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-erinvdoyle-equisync-uqcw06qmtq2.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
