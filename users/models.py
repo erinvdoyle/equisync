ce62c03c759af6295a5f9aea264198527d9bdb92
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-19 20:17:34 +0000

    add Profile model to models.py and update admin.py with UserAdmin and ProfileInline models for functionality between user registration and profile creation, relates to #29

--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+"""
+extends user class for automatic profile creation
+"""
+
+ROLE_CHOICES = [
+    ('Horse Owner', 'Horse Owner'),
+    ('Rider', 'Rider'),
+    ('Staff Member', 'Staff Member'),
+]
+
+user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, blank=True)
+role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=True, blank=True)
+horse = models.ForeignKey('Horse', on_delete=models.SET_NULL, null=True, blank=True)
+contact = models.CharField(max_length=100, blank=True)
+notes = models.TextField(blank=True)
+is_approved = models.BooleanField(default=False)
+
+
+def __str__(self):
+        return f"Profile for {self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    """
+    signal handler function to create a user profile when a new user is created.
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
