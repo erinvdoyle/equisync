f8d825d772f3986099ed5087f2d3e213cd731d79
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-19 22:33:56 +0000

    remove create_user_profile from models.py and add it to new file signals.py. Update UserConfig in apps.py with function ready to import signals, relates to #27

---
+++ users/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    signal to create a Profile when a new User is created
+    Source for explanation: Codu, as credited in README.md
+    """
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """
+    signal to save the Profile when the User is saved
+    """
+    instance.profile.save()
