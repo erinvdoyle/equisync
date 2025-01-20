860b5af7b42cc626a9d507229d3b8492463d4ea8
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-20 20:53:04 +0000

    install allauth and wire it up, relates to #27

--- equisync/urls.py
+++ equisync/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from users import views
  
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),  
