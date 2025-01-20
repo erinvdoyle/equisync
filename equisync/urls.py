e8c0ac914799d9ff9a25f5953523f0e5d05b5c55
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-20 12:16:34 +0000

    add profile.html to users/templates and modify settings.py, urls.py, relates to #29

--- equisync/urls.py
+++ equisync/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import my_test  
-
+from users import views
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', my_test, name='users'),  
+    # path('users/', my_test, name='users'),
+    path('profile/', views.view_profile, name='view_profile'),  
 ]
