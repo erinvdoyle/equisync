d516369349dc0b01877d7a9e1b769b786be0e05e
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-20 12:16:34 +0000

    add profile.html to users/templates and modify settings.py, urls.py, relates to #29

--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
-def my_test(request):
-    return HttpResponse("Test, test!")
+@login_required
+def view_profile(request):
+    """
+    gets the profile for the logged in user
+    """
+    profile, created = Profile.objects.get_or_create(user=request.user)
+    return render(request, 'users/profile.html', {'profile': profile})
