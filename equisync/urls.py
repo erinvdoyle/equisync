fad9d0897ab78d7d4848501154be09b36d7efb2c
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-20 14:57:22 +0000

    create edit_profile.html and forms.py, modify views.py, the link in profile.html, and add an urlpattern to urls.py to make profile editable, closes #29

--- equisync/urls.py
+++ equisync/urls.py
@@ -20,6 +20,6 @@ from users import views
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('users/', my_test, name='users'),
-    path('profile/', views.view_profile, name='view_profile'),  
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),  
 ]
