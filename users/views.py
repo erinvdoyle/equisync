e80e526f76fd1f80afb488bbd06ce084eb3074a4
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-20 14:57:22 +0000

    create edit_profile.html and forms.py, modify views.py, the link in profile.html, and add an urlpattern to urls.py to make profile editable, closes #29

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from .forms import ProfileForm
+
 
 # Create your views here.
 @login_required
@@ -10,3 +12,19 @@ def view_profile(request):
     """
     profile, created = Profile.objects.get_or_create(user=request.user)
     return render(request, 'users/profile.html', {'profile': profile})
+
+def edit_profile(request):
+    """
+    allows the logged in user to edit profile
+    """
+    profile = get_object_or_404(Profile, user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('view_profile')  
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, 'users/edit_profile.html', {'form': form})
