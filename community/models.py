e681f6bdfbaa06048237e364f538485c983e96bb
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-22 22:31:51 +0000

    create class Ad in community/models.py and add forms.py to app, relates to #23

--- community/models.py
+++ community/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Ad(models.Model):
+    AD_TYPE_CHOICES = [
+        ('for_sale', 'For Sale'),
+        ('wanted', 'Wanted'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
