82fc2df7aa136924f3be7a91bcb7f8ec5ad42aa1
Erin Doyle <erin.v.doyle@gmail.com>
2025-01-28 18:25:40 +0000

    update community.html and css to create functional ads and announcement sections as well as a placeholder events section, create and wire up static/js/script.js, relates to #10

--- community/views.py
+++ community/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .ads.models import Ad
+from .announcements.models import Announcement
 
 def community_overview(request):
     ads = Ad.objects.filter(approved=True)
+    announcements = Announcement.objects.filter(approved=True)
 
     return render(request, 'community/community.html', {
         'ads': ads,
+        'announcements': announcements,
     })
