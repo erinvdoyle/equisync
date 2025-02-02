64c35f4956ded90f60e69ceb62c4ec2eccbf01de
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-02 10:30:02 +0000

    tweak display and settings of sales ad section of community.html for different screen sizes; upload sales ad images

--- community/ads/forms.py
+++ community/ads/forms.py
@@ -4,4 +4,4 @@ from .models import Ad
 class AdForm(forms.ModelForm):
     class Meta:
         model = Ad
-        fields = ['ad_type', 'image', 'description', 'contact_info', 'price']  
+        fields = ['ad_type', 'image', 'category', 'description', 'contact_info', 'price']  
