63af2e64f434bababd79e2c5b1e15c40bfe1de82
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-03 12:07:54 +0000

    update navbar with working links to home and community pages

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import view_profile, edit_profile, index
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='home'),
     path('profile/', view_profile, name='view_profile'),
     path('profile/edit/', edit_profile, name='edit_profile'),
 ]
