f2257624bd49c1df6ce8aec159d2e1f6334f07a4
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-03 11:27:37 +0000

    add label feature to for sale and wanted ads, update search and filter fields in ads toolbar, relates to #23

--- community/filters.py
+++ community/filters.py
@@ -1,9 +1,13 @@
 import django_filters
 from community.ads.models import Ad
 
+CATEGORY_CHOICES = [
+        ("Horse", "Horse"), ("Equipment", "Equipment"), ("Clothing", "Clothing"), ("Machinery/Auto", "Machinery/Auto"), ("Animal", "Animal"), ("Other", "Other"),
+    ]
+
 class AdFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(field_name='description', lookup_expr='icontains', label='Search')
-    category = django_filters.ChoiceFilter(choices=[("Horse", "Horse"), ("Equipment", "Equipment"), ("Clothing", "Clothing"), ("Machinery/Auto", "Machinery/Auto"), ("Animal", "Animal"), ("Other", "Other")], label='Category')
+    category = django_filters.ChoiceFilter(choices=CATEGORY_CHOICES, label="Category")
 
     class Meta:
         model = Ad
