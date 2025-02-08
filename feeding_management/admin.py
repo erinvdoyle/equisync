ab2c4b54bfcbc5599d385bf6c7f084b75e634d38
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-08 12:27:09 +0000

    update feeding_management app admin and both tempaltes so that horse feeding charts can be created, edited, and viewed by users, relates to #22@

--- feeding_management/admin.py
+++ feeding_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FeedingChart
 
-# Register your models here.
+
+class FeedingChartAdmin(admin.ModelAdmin):
+    list_display = ('horse', 'breakfast_feed', 'lunch_feed', 'dinner_feed', 'hay', 'supplements', 'medicines', 'notes')
+    
+
+admin.site.register(FeedingChart, FeedingChartAdmin)
