7284b0bcd6b88afdd3e567b0532f4924dff73678
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-09 09:43:27 +0000

    update feeding_management forms.py and views.py to allow approved users to create, edit, and view individual horse feeding charts, relates to #22

--- feeding_management/forms.py
+++ feeding_management/forms.py
@@ -1,7 +1,13 @@
-# from django import forms
-# from .models import FeedingChart
+from django import forms
+from .models import FeedingChart
 
-# class FeedingChartForm(forms.ModelForm):
-#     class Meta:
-#         model = FeedingChart
-#         fields = ['breakfast_feed', 'breakfast_quantity', 'lunch_feed', 'lunch_quantity', 'dinner_feed', 'dinner_quantity', 'hay', 'hay_quantity', 'supplements', 'medicines', 'notes', 'approved_users']
+class FeedingChartForm(forms.ModelForm):
+    class Meta:
+        model = FeedingChart
+        fields = [
+            'breakfast_feed', 'breakfast_quantity',
+            'lunch_feed', 'lunch_quantity',
+            'dinner_feed', 'dinner_quantity',
+            'hay', 'hay_quantity',
+            'supplements', 'medicines', 'notes'
+        ]
