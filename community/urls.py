19dfac4d428664aed688314eef33f191967625d7
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-10 17:06:18 +0000

    update base.html and dashboard.html as well as all neccessary views.py and urls.py to create working links and dashboard functionality. Add ad_detail.html and announcement_detail.html so that users may view their community posts through the dashboard. Relates to #6 sub-issue

--- community/urls.py
+++ community/urls.py
@@ -1,19 +1,28 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .ads.views import submit_ad, edit_ad
 from .announcements.views import submit_announcement, edit_announcement  
 from .views import community_overview
+from .ads import views as ads_views
+from .announcements import views as announcement_views
+
 # from django.conf import settings
 # from django.conf.urls.static import static  
 
 app_name = 'community'
 
 urlpatterns = [
-    path('ads/submit/', submit_ad, name='submit_ad'),
-    path('ads/edit/<int:ad_id>/', edit_ad, name='edit_ad'),
-    path('announcements/edit/<int:announcement_id>/', edit_announcement, name='edit_announcement'),
-    path('announcements/submit/', submit_announcement, name='submit_announcement'),
-    path('', community_overview, name='community_overview'),  
+    path('ads/', include([
+        path('submit/', ads_views.submit_ad, name='submit_ad'),
+        path('edit/<int:ad_id>/', ads_views.edit_ad, name='edit_ad'),
+        path('<int:ad_id>/', ads_views.ad_detail, name='ad_detail'),
+    ])),
+    path('announcements/', include([
+        path('submit/', announcement_views.submit_announcement, name='submit_announcement'),
+        path('edit/<int:announcement_id>/', announcement_views.edit_announcement, name='edit_announcement'),
+        path('<int:announcement_id>/', announcement_views.announcement_detail, name='announcement_detail'),
+    ])),
+    path('', views.community_overview, name='community_overview'),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
