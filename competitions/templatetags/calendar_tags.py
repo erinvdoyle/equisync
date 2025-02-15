376a62e51dd63bcdeb437dc728a6267f5db1dfbd
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-15 18:18:18 +0000

    update 'competitions' with FullCalendar functionality. Make events viewable by month, week, or day, and clickable for more information. Add tags to calendar_tags and update views. Add js and css for calendar look and function. Relates to #8

--- competitions/templatetags/calendar_tags.py
+++ competitions/templatetags/calendar_tags.py
@@ -5,10 +5,40 @@ from competitions.models import Event
 
 register = template.Library()
 
+class EventCalendar(calendar.HTMLCalendar):
+    def __init__(self, events, year, month):
+        super().__init__()
+        self.events = events
+        self.year = year
+        self.month = month
+
+    def formatday(self, day, weekday):
+        """
+        Return a day as a table cell.
+        """
+        if day == 0:
+            return '<td class="noday">&nbsp;</td>'
+        else:
+            today = date(self.year, self.month, day)
+            day_events = [event for event in self.events if event.start_time.date() <= today <= event.end_time.date()]
+            event_links = ''.join(f'<div><a href="{event.get_absolute_url()}">{event.title}</a></div>' for event in day_events)
+
+            return f'<td class="{self.cssclasses[weekday]}">{day}{event_links}</td>'
+    
+    def formatweekheader(self):
+        """
+        Return a header for a week as a table row.
+        """
+        s = ''.join('<th class="%s">%s</th>' % (self.cssclasses[i], calendar.weekheader(3)[i]) for i in range(7))
+        return '<tr class="days">%s</tr>' % s
+
 @register.simple_tag
-def month_calendar(year, month):
-    cal = calendar.HTMLCalendar().formatmonth(year, month)
-    return cal
+def month_calendar(year, month, events):
+    cal = EventCalendar(events, year, month)
+    html_calendar = cal.formatmonth(year, month)
+    html_calendar = html_calendar.replace('<table', '<table class="calendar"')
+    
+    return html_calendar
 
 @register.simple_tag
 def current_day():
