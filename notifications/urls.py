98a9f0d48b250dfd350169b5602f7ef2f8b62779
erinvdoyle <erin.v.doyle@gmail.com>
2025-02-26 11:21:13 +0000

    add two notifications templates to notifications app: notification_detail.html and notifications_list.html. Update views.py with notification views

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import mark_as_read
-from .views import notifications_list
+from .views import notifications_list, notification_detail
 
 urlpatterns = [
     path("mark-read/<int:notification_id>/", mark_as_read, name="mark_as_read"),
     path('', notifications_list, name='notifications_list'),
+    path('<int:notification_id>/', notification_detail, name='notification_detail'),
 ]
