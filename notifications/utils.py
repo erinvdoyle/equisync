f3e74ce1dad272d9a4061625eea9b37b95e17d0a
erinvdoyle <erin.v.doyle@gmail.com>
2025-03-01 11:29:54 +0000

    create signals.py in community app to trigger user notifications when a user comments on another user's community post

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,5 +1,5 @@
 from .models import Notification
 
-def send_notification(user, message):
+def send_notification(user, message, event_horse=None):
     """Creates a notification for a specific user."""
-    Notification.objects.create(user=user, message=message)
+    Notification.objects.create(user=user, message=message, event_horse=event_horse)
