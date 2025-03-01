e8a35ffa1879792985c997a8a7d5d96fd13cf6fa
erinvdoyle <erin.v.doyle@gmail.com>
2025-03-01 11:29:54 +0000

    create signals.py in community app to trigger user notifications when a user comments on another user's community post

---
+++ community/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Comment
+from notifications.utils import send_notification
+
+@receiver(post_save, sender=Comment)
+def notify_comment(sender, instance, created, **kwargs):
+    if created:
+        recipient = instance.content_object.user
+        message = f"{instance.user.username} commented on your post: '{instance.content_object}'"
+        send_notification(recipient, message)
