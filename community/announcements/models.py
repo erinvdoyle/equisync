24fdf11032a9da195d2679cebfd43eec687149e8
erinvdoyle <erin.v.doyle@gmail.com>
2025-03-20 19:04:41 +0000

    update Announcement model and community.html and admin so that announcements employ cloudinary

--- community/announcements/models.py
+++ community/announcements/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from cloudinary.models import CloudinaryField
 
 class Announcement(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='announcements/images', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     description = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     contact_info = models.CharField(max_length=255)
